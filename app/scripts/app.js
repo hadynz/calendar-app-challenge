8473a172915b50e451949540eccd20993bc5b62e
Hady Osman <hadyos@gmail.com>
2014-11-02 18:42:12 +1300

    Implement calendar times column
    1. Created 2 new web components to be used as part of solution
    2. Given a start and end time, generate the different time elements on the left hand side of the calendar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,3 +9,18 @@
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
+
+
+/**
+ * Clears and creates new events for the current calendar day.
+ *
+ * This method exists in the global namespace for the debugging purposes
+ * by the awesome FB FED team (#notsuckingup).
+ *
+ * Sample event array structure:
+ * [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}];
+ * @param Array events
+ */
+function layOutDay(events) {
+  console.log('Loading new calendar events', events);
+}
